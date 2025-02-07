bb1fcc7b07b59927fa240c229c197f698d0339a0
colinpilet <colin.pilet@etu.univ-orleans.fr>
2025-02-07 17:59:59 +0100

    Ajout de l'entiter player

--- apps/realtime-elo-ranker-server/dist/app.module.js
+++ apps/realtime-elo-ranker-server/dist/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const player_service_1 = require("./player/player.service");
@@ -17,7 +18,14 @@ let AppModule = class AppModule {
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'sqlite',
+                database: 'db',
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService, player_service_1.PlayerService, match_service_1.MatchService,],
     })
